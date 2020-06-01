a9f121a6f01c6fdc0910e45321c3f6c63207c6a3
Eric <ericgmicaela@gmail.com>
2020-06-01 09:57:07 -0300

    MODULO IDADE V3.0

--- idade.py
+++ idade.py
@@ -7,6 +7,7 @@ MES = idade.mes(arg)
 ANO = idade.ano(arg)
 
 ##ERROS:
+Se o formado digitado for diferente do pedido: Ele dira que a data é incorreta e retorna -1 no braço ano.
 Se a data for futura: Ele dira que a data é incorreta e retorna -1 no braço ano.
 Se for um dia que o mês não tem: Ele avisa que o mes tem determinado limite e retorna -1 no braço ano.
 
@@ -17,9 +18,21 @@ from datetime import datetime
 import calendar
 
 def ano (nascimento):
-    dia = int(nascimento[0:2])
-    mes = int(nascimento[3:5])
+    dia = (nascimento[0:2])
+    if dia in '00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31':
+        dia = int(dia)
+    else:
+        print("\033[1;31mInforme uma data correta!\033[;m")
+        return -1
+    mes = (nascimento[3:5])
+    if mes in '00,01,02,03,04,05,06,07,08,09,10,11,12':
+        mes = int(mes)
+    else:
+        print("\033[1;31mInforme uma data correta!\033[;m")
+        return  -1
     ano = int(nascimento[6:10])
+    if ano < 1000:
+        ano += 2000
     idade_ano = idade_mes = idade_dia = 0
 
     ## Verifica se a pessoa ja nasceu e se a data é possivel
